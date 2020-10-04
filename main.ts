9909a7bb4e0354eacc419060f68519958ce40e5b
Madlife2612 <72304511+Madlife2612@users.noreply.github.com>
2020-10-04 07:28:00 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,66 @@
-basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    for (let Green = 0; Green <= 10; Green++) {
-        basic.showNumber(10 - Green)
-        basic.pause(1000)
-    }
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    for (let Yellow = 0; Yellow <= 3; Yellow++) {
-        basic.showNumber(3 - Yellow)
-        basic.pause(1000)
-    }
+function RED () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P0, 1)
     for (let Red = 0; Red <= 13; Red++) {
         basic.showNumber(13 - Red)
         basic.pause(1000)
     }
+}
+function GREEN () {
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    for (let Green = 0; Green <= 10; Green++) {
+        basic.showNumber(10 - Green)
+        basic.pause(1000)
+    }
+}
+function GREEN2 () {
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    for (let Green = 0; Green <= 10; Green++) {
+        basic.showNumber(10 - Green)
+        basic.pause(1000)
+    }
+}
+function RED2 () {
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P12, 1)
+    for (let Red = 0; Red <= 13; Red++) {
+        basic.showNumber(13 - Red)
+        basic.pause(1000)
+    }
+}
+function YELLOW2 () {
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    for (let Yellow = 0; Yellow <= 3; Yellow++) {
+        basic.showNumber(3 - Yellow)
+        basic.pause(1000)
+    }
+}
+function YELLOW () {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    for (let Yellow = 0; Yellow <= 3; Yellow++) {
+        basic.showNumber(3 - Yellow)
+        basic.pause(1000)
+    }
+}
+basic.forever(function () {
+    if (pins.digitalReadPin(DigitalPin.P2) == 1) {
+        pins.digitalWritePin(DigitalPin.P12, 1)
+    } else if (pins.digitalReadPin(DigitalPin.P12) == 1) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    }
+    if (pins.digitalReadPin(DigitalPin.P14) == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else if (pins.digitalReadPin(DigitalPin.P0) == 1) {
+        pins.digitalWritePin(DigitalPin.P14, 1)
+    }
+    GREEN()
+    YELLOW()
+    RED()
+    GREEN2()
+    YELLOW2()
+    RED2()
 })
